b0e2ed105ede3fbf13fb5b2c915b0bf5505d187c
Tomek4861 <Tomek4861g@gmail.com>
2025-01-12 00:42:17 +0100

    Refactored registration & login pages

--- tracker/context_processors.py
+++ tracker/context_processors.py
@@ -1,7 +1,10 @@
 def navbar_data(request):
+    homepage_dict = dict(name="Main Page", icon="bi-house", url="/")
     navbar_from_request_origin = {
         "/": dict(name="Portfolio", icon="bi-wallet2", url="/portfolio"),
-        '/portfolio/': dict(name="Main Page", icon="bi-house", url="/"),
+        '/portfolio/': homepage_dict,
+        "/login/": homepage_dict,
+        "/register/": homepage_dict,
     }
     ret = navbar_from_request_origin.get(request.path, dict(name="Error", icon="", url=""))
     # print("returning", ret)
